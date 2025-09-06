ebd741cbd7602986d03792fac2a21014a9746ff7
niharikaty <niharikaty07@gmail.com>
2025-09-06 23:55:35 +0530

    Add files via upload

---
+++ code h.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+    int n = 3;  
+    for(i = 1; i <= n; i++) {
+        for(space = 1; space <= n - i; space++) {
+            printf("  ");
+        }
+        for(j = 1; j <= (2 * i - 1); j++) {
+            if(j % 2 == 1)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+    for(i = n - 1; i >= 1; i--) {
+        for(space = 1; space <= n - i; space++) {
+            printf("  ");
+        }
+        for(j = 1; j <= (2 * i - 1); j++) {
+            if(j % 2 == 1)
+                printf("* ");
+            else
+                printf("  "); 
+}
+        printf("\n");
+    }
+    return 0;
+}
